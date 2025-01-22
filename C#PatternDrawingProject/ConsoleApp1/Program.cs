68a74c22fa64186b4c39653cf6eff9c71207b9fc
Mihail <mishomihov41@gmail.com>
2025-01-23 00:19:29 +0200

    Added more shapes

--- C#PatternDrawingProject/ConsoleApp1/Program.cs
+++ C#PatternDrawingProject/ConsoleApp1/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-class PatternDrawing
+internal class PatternDrawing
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        while (true) // Loop for restarting the program
+        bool restart = true;
+        while (restart) // Loop for restarting the program
         {
             // Step 1: Display a menu to the user
             Console.WriteLine("Welcome to the C# Pattern Drawing Program!");
@@ -15,7 +16,8 @@ class PatternDrawing
             Console.WriteLine("4. Left-angled Triangle");
             Console.WriteLine("5. Pyramid");
             Console.WriteLine("6. Reverse Pyramid");
-            Console.WriteLine("7. Rectangle with Hollow Center");
+            Console.WriteLine("7. Heart");
+            Console.WriteLine("8. Rectangle with Hollow Center");
 
             // Step 2: Get the user's choice
             Console.Write("Enter the number corresponding to your choice: ");
@@ -23,12 +25,12 @@ class PatternDrawing
 
             // Step 3: Get dimensions based on choice
             int rows = 0, width = 0, height = 0;
-            if (choice >= 1 && choice <= 6)
+            if (choice >= 1 && choice <= 7)
             {
                 Console.Write("Enter the amount of rows: ");
                 rows = int.Parse(Console.ReadLine());
             }
-            else if (choice == 7)
+            else if (choice == 8)
             {
                 Console.Write("Enter the height of the rectangle: ");
                 height = int.Parse(Console.ReadLine());
@@ -50,9 +52,7 @@ class PatternDrawing
                 {
                     continue;
                 }
-
             }
-
 
             // Step 4: Generate the selected pattern
             switch (choice)
@@ -66,6 +66,7 @@ class PatternDrawing
                         }
                         Console.Write("\n");
                     }
+                    restart = false;
                     break;
 
                 case 2: // Square with Hollow Center
@@ -84,10 +85,40 @@ class PatternDrawing
                         }
                         Console.WriteLine();
                     }
+                    restart = false;
                     break;
 
                 case 3: // Diamond
-                    // TODO: Use nested loops to create a diamond shape
+                    for (int i = 0; i <= rows; i++)
+                    {
+                        for (int j = 1; j <= rows - i; j++)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = 1; j <= 2 * i - 1; j++)
+                        {
+                            Console.Write("*");
+                        }
+
+                        Console.Write("\n");
+                    }
+
+                    for (int i = rows - 1; i >= 1; i--)
+                    {
+                        for (int j = 1; j <= rows - i; j++)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = 1; j <= 2 * i - 1; j++)
+                        {
+                            Console.Write("*");
+                        }
+
+                        Console.Write("\n");
+                    }
+                    restart = false;
                     break;
 
                 case 4: // Left-angled Triangle
@@ -100,13 +131,10 @@ class PatternDrawing
                         }
                         Console.Write("\n");
                     }
+                    restart = false;
                     break;
 
-                case 5: // Hollow Square
-                    // TODO: Use nested loops to create a hollow square
-                    break;
-
-                case 6: // Pyramid
+                case 5: // Pyramid
                     for (int i = 1; i <= rows; i++)
                     {
                         for (int x = i; x <= rows; x++)
@@ -119,9 +147,10 @@ class PatternDrawing
                         }
                         Console.WriteLine();
                     }
+                    restart = false;
                     break;
 
-                case 7: // Reverse Pyramid
+                case 6: //  Reverse pyramid
                     for (int i = rows; i >= 0; i--)
                     {
                         for (int x = i; x <= rows; x++)
@@ -134,7 +163,50 @@ class PatternDrawing
                         }
                         Console.WriteLine();
                     }
+                    restart = false;
+                    break;
 
+                case 7: // Heart
+                    for (int i = rows / 2; i <= rows; i += 2)
+                    {
+                        for (int j = 1; j < rows - i; j += 2)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = 1; j <= i; j++)
+                        {
+                            Console.Write("*");
+                        }
+
+                        for (int j = 1; j <= rows - i; j++)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = 1; j <= i; j++)
+                        {
+                            Console.Write("*");
+                        }
+
+                        Console.WriteLine();
+                    }
+
+                    for (int i = rows; i >= 1; i--)
+                    {
+                        for (int j = i; j < rows; j++)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = 1; j <= (i * 2) - 1; j++)
+                        {
+                            Console.Write("*");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    restart = false;
                     break;
 
                 case 8: // Rectangle with Hollow Center
@@ -145,9 +217,6 @@ class PatternDrawing
                     Console.WriteLine("❌ Invalid choice! Please restart the program.");
                     break;
             }
-
-            // Step 5: Optional - Allow the user to restart or exit
-      
         }
     }
 }
--- C#PatternDrawingProject/Test/Program.cs
+++ C#PatternDrawingProject/Test/Program.cs
@@ -1,27 +1,53 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Test
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            int n = 6; // Size of the heart
 
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            for (int i = n / 2; i <= n; i += 2)
             {
-                for (int x = i; x <= n; x++)
+                for (int j = 1; j < n - i; j += 2)
                 {
                     Console.Write(" ");
                 }
+
                 for (int j = 1; j <= i; j++)
                 {
-                    Console.Write("*" + " ");
+                    Console.Write("*");
                 }
+
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= (i * 2) - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
                 Console.WriteLine();
             }
         }
-
-
     }
 }
